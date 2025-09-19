c2c4dc42189b7843cf2ea7f19e170d9ad4ee49e1
LinusBwana <linusbwana49@gmail.com>
2025-09-19 14:20:18 +0300

    Create Property, Booking, and Review models and serializers

---
+++ alx_travel_app/listings/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Property, Booking
+
+
+class PropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = "__all__"
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = "__all__"
